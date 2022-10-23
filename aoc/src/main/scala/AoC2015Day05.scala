4700f345d1401ac80a4882f1000dfda3429c066f
Bram <bram.boogaarts@gmail.com>
2022-10-23 18:23:51 +0200

    Day 05 scala part 2 WiP

--- aoc/src/main/scala/AoC2015Day05.scala
+++ aoc/src/main/scala/AoC2015Day05.scala
@@ -12,48 +12,37 @@ object AoC2015Day05 extends App:
 
         val prohibitedStrings: Set[String] = Set("ab", "cd", "pq", "xy")
 
-        val pairs: Iterator[String] = str.sliding(2)
-
         def threeVowels(): Boolean =
             str.filter(char => vowels.contains(char)).length >= 3
         
         def twoLettersInRow(): Boolean =
-            val pairs1: Seq[String] =
-                for i <- 0 until str.length - 1 if str(i) == str(i+1)
-                yield str(i).toString.appended(str(i+1))
-
-            pairs1.length > 0
+            str.sliding(2).map(s => s(0) == s(1)).contains(true)
 
         def noProhibitedSubstrings(): Boolean =
-            val subs: Set[String] =
-                for sub <- prohibitedStrings if str.contains(sub)
-                yield sub
-                
-            subs.size == 0
+            ! str.sliding(2).map(s => prohibitedStrings.contains(s)).contains(true)
 
         def isNice1(): Boolean =
             threeVowels() & twoLettersInRow() & noProhibitedSubstrings()
 
-        def twoLettersInRowAlt(): Boolean =
-            pairs.map(s => s(0) == s(1)).contains(true)
+        def hasRecurringPair(): Boolean =
+            val pairsWithIndices: List[(String, (Int, Int))] =
+                str.sliding(2)
+                .zipWithIndex
+                .map((s, idx) => (s, (idx, idx+1)))
+                .toList
 
-        def noProhibitedSubstringsAlt(): Boolean =
-            println(pairs.toList);
-            ! pairs.map(s => prohibitedStrings.contains(s)).contains(true)
+            println(pairsWithIndices)
+            
+            val pairsGrouped = pairsWithIndices
+            .groupBy(_(0))
+            .map((pair, pairsWithIdxList) => (pair, pairsWithIdxList(0).toList))//.fold(Set())((set, idx) => set.include(idx))))
 
-        def isNice1Alt(): Boolean =
-            threeVowels() & twoLettersInRowAlt() & noProhibitedSubstringsAlt()
+            println(pairsGrouped.toList)
 
-        def recurringPair(): Boolean =
-            ???
+            true
 
     //val answer1: Int = strings.map(Word(_).isNice1()).count(_ == true)
 
     //println(s"Result part 1: ${answer1}")
-    val word = Word(strings(123))
 
-    //println(strings.map(s => (Word(s), Word(s).isNice1Alt())).diff(strings.map(s => (Word(s), Word(s).isNice1()))))
-    println(word.pairs.toList)
-    println(! word.pairs.map(s => word.prohibitedStrings.contains(s)).contains(true))
-    println(word.pairs.toList)
-    println(word.noProhibitedSubstringsAlt())
+    println("xyxy".sliding(2).zipWithIndex.map((s, idx) => (s, (idx, idx+1))).toList)//(0)(0))
