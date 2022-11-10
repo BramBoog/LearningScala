42c58c9076162bcfa5b95bf74a8b24c9db0f7764
Bram Boogaarts <bram.boogaarts@gmail.com>
2022-11-10 16:53:04 +0100

    all exercises chapter 3 and first execercise chapter 2

--- fps/src/main/scala/part1/Chapter3.scala
+++ fps/src/main/scala/part1/Chapter3.scala
@@ -65,7 +65,7 @@ object List:
     def product(l: List[Double]): Double =
         foldLeft(l, 1.0)(_*_)
 
-    def lengthAlt[A](l: List[A]): Int =
+    def lengthViaFoldLeft[A](l: List[A]): Int =
         foldLeft(l, 0)((x,y) => x+1)
 
     // Exercise 3.12
@@ -73,10 +73,10 @@ object List:
         foldLeft(l, Nil: List[A])((lst, el) => Cons(el, lst))
 
     // Exercise 3.13
-    def foldLeftAlt[A,B](as: List[A], z: B)(f: (B,A) => B): B =
+    def foldLeftViaFoldRight[A,B](as: List[A], z: B)(f: (B,A) => B): B =
         foldRight(List.reverse(as), z)((x,y) => f(y,x))
 
-    def foldRightAlt[A,B](as: List[A], z: B)(f: (A,B) => B): B =
+    def foldRightViaFoldLef[A,B](as: List[A], z: B)(f: (A,B) => B): B =
         foldLeft(List.reverse(as), z)((x,y) => f(y, x))
 
     // Exercise 3.14
@@ -110,9 +110,81 @@ object List:
         concat(map(as)(f))
 
     // Exercise 3.21
-    def filterAlt[A](as: List[A])(f: A => Boolean): List[A] =
+    def filterViaFlatMap[A](as: List[A])(f: A => Boolean): List[A] =
         flatMap(as)(a => if (f(a)) List(a) else Nil)
 
     // Exercise 3.22
     def addElementWise(l1: List[Int], l2: List[Int]): List[Int] =
-        ???
+        (l1, l2) match
+            case (Nil, Nil) => Nil
+            case (Nil, Cons(h, t)) => throw new Exception("The second list is longer than the first.")
+            case (Cons(h, t), Nil) => throw new Exception("The first list is longer than the second.")
+            case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addElementWise(t1, t2))
+
+    // Exercise 3.23
+    def zipWith[A, B, C](l1: List[A], l2: List[B], op: (A, B) => C): List[C] =
+        (l1, l2) match
+            case (Nil, Nil) => Nil
+            case (Nil, Cons(h, t)) => throw new Exception("The second list is longer than the first.")
+            case (Cons(h, t), Nil) => throw new Exception("The first list is longer than the second.")
+            case (Cons(h1, t1), Cons(h2, t2)) => Cons(op(h1, h2), zipWith(t1, t2, op))
+
+    // Exercise 3.24
+    def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean =
+        if (length(sup) < length(sub)) throw new IllegalArgumentException("The length of the sublist is longer than the list to search.")
+        else
+            def makeSliding(inputL: List[A]): List[List[A]] =
+                def makeSlidingSub(l: List[A], i: Int): List[A] =
+                    if (i == length(sub)) Nil
+                    else Cons(head(l), makeSlidingSub(tail(l), i+1))
+                
+                if (length(inputL) < length(sub)) Nil
+                else Cons(makeSlidingSub(inputL, 0), makeSliding(tail(inputL)))
+            
+            length(dropWhile(makeSliding(sup), _!=sub)) > 0
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+// Exercise 3.25
+def size[A](t: Tree[A]): Int =
+    t match
+        case Leaf(a) => 1
+        case Branch(l, r) => 1 + size(l) + size(r)
+
+// Exercise 3.26
+def maximum(t: Tree[Int]): Int =
+    t match
+        case Leaf(a) => a
+        case Branch(l, r) => maximum(l).max(maximum(r))
+
+// Exercise 3.27
+def depth[A](t: Tree[A]): Int =
+    t match
+        case Leaf(a) => 1
+        case Branch(l, r) => 1 + depth(l).max(depth(r))
+
+// Exercise 3.28
+def map[A, B](t: Tree[A], f: A => B): Tree[B] =
+    t match
+        case Leaf(a) => Leaf(f(a))
+        case Branch(l, r) => Branch(map(l, f), map(r, f))
+
+// Exercise 3.29
+def fold[A, B](t: Tree[A], f: A => B, g: (B, B) => B): B =
+    t match
+        case Leaf(a) => f(a)
+        case Branch(l, r) => g(fold(l, f, g), fold(r, f, g))
+
+def sizeViaFold[A](t: Tree[A]): Int =
+    fold(t, _ => 1, (l, r) => 1 + l + r)
+
+def maximumViaFold(t: Tree[Int]): Int =
+    fold(t, identity, (l, r) => l.max(r))
+
+def depthViaFold[A](t: Tree[A]): Int =
+    fold(t, _ => 1, (l, r) => 1 + l.max(r))
+
+def mapViaFold[A, B](t: Tree[A], f: A => B): Tree[B] =
+    fold[A, Tree[B]](t, a => Leaf(f(a)), (l, r) => Branch(l, r))
