5b7cf0bd1b611910e6a6cbf2fafdd0b4537172d4
Bram Boogaarts <bram.boogaarts@gmail.com>
2022-11-28 17:27:20 +0100

    Day07 scala complete

--- aoc/src/main/scala/AoC2015Day07.scala
+++ aoc/src/main/scala/AoC2015Day07.scala
@@ -20,9 +20,25 @@ object AoC2015Day07 extends App:
             getWireValueByName(wiresMap, inputName1)
             .flatMap(x => getWireValueByName(wiresMap, inputName2).map(op.curried(x)))
 
+    def solve(forWire: String, instructions: List[Instruction], wireOverrides: Option[Map[String, Int]] = None): Int =
+        @annotation.tailrec
+        def runInstructions(wires: Map[String, Int], instr: List[Instruction]): Int =
+            wires.get(forWire) match
+                case Some(a) => a
+                case None =>
+                    instr.head.evaluate(wires) match
+                        case None => runInstructions(wires, instr.tail.appended(instr.head))
+                        case Some(x) => runInstructions(wires + (instr.head.retName -> x), instr.tail)
+
+        wireOverrides match
+            case None => runInstructions(Map(), instructions)
+            case Some(m) => runInstructions(m, instructions.filter(i => !m.keySet.contains(i.retName)))
+
     def parseInstruction(str: String): Instruction =
         str match
+            case s"${a} AND ${b} -> ${r}" if a.toIntOption.isDefined => singleInput(r, b, x => 1 & x)
             case s"${a} AND ${b} -> ${r}" => doubleInput(r, a, b, (x,y) => x & y)
+            case s"${a} OR ${b} -> ${r}" if a.toIntOption.isDefined => singleInput(r, b, x => 1 | x)
             case s"${a} OR ${b} -> ${r}" => doubleInput(r, a, b, (x,y) => x | y)
             case s"${a} LSHIFT ${b} -> ${r}" => singleInput(r, a, x => x << b.toInt)
             case s"${a} RSHIFT ${b} -> ${r}" => singleInput(r, a, x => x >> b.toInt)
@@ -36,22 +52,15 @@ object AoC2015Day07 extends App:
             case s"${a} -> ${r}" => singleInput(r, a, identity)
             case _ => sys.error(s"Input not defined: ${str}")
 
-    @annotation.tailrec
-    def solve(forWire: String, wires: Map[String, Int], instr: List[Instruction]): Int =
-        println(instr.length)
-        wires.get(forWire) match
-            case Some(answ) => answ
-            case None => 
-                instr.head.evaluate(wires) match
-                    case None => solve(forWire, wires, instr.tail.appended(instr.head))
-                    case Some(x) => solve(forWire, wires + (instr.head.retName -> x), instr.tail)
-
     val instructions: List[Instruction] =
         Source.fromResource("2015/InputDay07.txt")
         .getLines
         .toList
         .map(parseInstruction)
 
-    val answer1: Int = solve("a", Map(), instructions)
 
+    val answer1: Int = solve("a", instructions)
     println(s"Result part 1: ${answer1}")
+
+    val answer2: Int = solve("a", instructions, Some(Map(("b" -> answer1))))
+    println(s"Result part 2: ${answer2}")
