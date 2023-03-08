5dda0fb8308cbc31b94c1a1e2871ed3151ac0c7f
Bram Boogaarts <bram.boogaarts@gmail.com>
2023-03-08 16:24:51 +0100

    Finished day08

--- aoc/src/main/scala/AoC2015/AoC2015Day08.scala
+++ aoc/src/main/scala/AoC2015/AoC2015Day08.scala
@@ -6,28 +6,30 @@ object AoC2015Day08 extends App:
         .getLines
         .toList
 
-    def countInMemChars(cs: List[Char], acc: Int = 0): Int =
-        val hexChars = Set('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f')
-        cs match
-            case Nil => acc
-            case '\\' :: 'x' :: c :: d :: l if (Set(c, d).intersect(hexChars).size == 2) => countInMemChars(l, acc + 1)
-            case '\\' :: '\\' :: l => countInMemChars(l, acc + 1)
-            case '\\' :: '\"' :: l => countInMemChars(l, acc + 1)
-            case '\"' :: l => countInMemChars(l, acc)
-            case _ => countInMemChars(cs.tail, acc + 1)
-
     def unescaped(s: String): String =
         def loop(todo: List[Char], ret: String = ""): String =
             todo match
                 case Nil => ret
-                case '\\' :: '\\' :: rest => loop(rest, ret + '\\')
-                case '\\' :: '\"' :: rest => loop(rest, ret + '\"')
-                case '\\' ::  'x' :: h1 :: h2 :: rest => loop(rest, ret + Integer.parseInt(s"$h1$h2", 16).toChar)
-                case c1 :: rest => loop(rest, ret + c1)
+                case '\\' :: '\\' :: lst => loop(lst, ret + '\\')
+                case '\\' :: '\"' :: lst => loop(lst, ret + '\"')
+                case '\"' :: lst => loop(lst, ret)
+                case '\\' ::  'x' :: c :: d :: lst => loop(lst, ret + Integer.parseInt(s"$c$d", 16).toChar)
+                case c :: lst => loop(lst, ret + c)
 
         loop(s.drop(1).dropRight(1).toList)
+
+    def escaped(s: String): String =
+        def loop(todo: List[Char], ret: String = "\""): String =
+            todo match
+                case Nil => ret + '\"'
+                case '\\' :: lst => loop(lst, ret + "\\\\")
+                case '\"' :: lst => loop(lst, ret + "\\\"")
+                case c :: lst => loop(lst, ret + c)
+
+        loop(s.toList)            
     
-    val answer1: Int = input.map(s => s.length - countInMemChars(s.toList)).sum
+    val answer1: Int = input.map(s => s.length - unescaped(s).length).sum
     println(s"Result part 1: ${answer1}")
-    val answer1a: Int = input.map(s => s.length - unescaped(s).length).sum
-    println(s"Result part 1: ${answer1}")
+
+    val answer2: Int = input.map(s => escaped(s).length - s.length).sum
+    println(s"Result part 2: ${answer2}")
